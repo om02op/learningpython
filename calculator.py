89d4137ce0a9798fd309de962d5a7dab3ffbb730
om02op <minecraftomislive@gmail.com>
2022-05-27 22:30:01 +0530

    Calculator, Day 2 update

---
+++ calculator.py
@@ -0,0 +1,19 @@
+first =  input( "Enter your first number: ")
+operator = input("Enter your operator: ")
+second = input("Enter your second number: ")
+
+first = int(first)
+second = int(second)
+
+if operator == "+":
+    print(first + second)
+elif operator == "-":
+    print(first - second)
+elif operator == "*":
+    print(first * second)
+elif operator == "/":
+    print(first / second)
+elif operator == "%":
+    print(first % second)
+else:
+     print("Invalid Operation")           
