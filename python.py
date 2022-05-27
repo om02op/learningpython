71e7f0993464016234db8336c992cdf25fa31e44
om02op <minecraftomislive@gmail.com>
2022-05-27 19:36:17 +0530

    Enter

--- python.py
+++ python.py
@@ -1,1 +1,10 @@
-print(2 / 13)
+age = 1
+
+if age >= 18:
+   print("you are an adult")
+   print("you can vote")
+elif age < 3:
+   print("you are a child")
+else:
+   print("you are in school")
+print("thank you") 
